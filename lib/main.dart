53569ee80d63d010dd1ae722297735fd46ca1a2c
X <129300554+x6455@users.noreply.github.com>
2026-07-02 09:13:56 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,22 +5,34 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    // Call immediately when app launches
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _makeCall();
+    });
+  }
+
+  void _makeCall() {
+    const channel = MethodChannel('com.example.hello_world/phone');
+    channel.invokeMethod('callNumber', {'number': '+1234567890'});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              // Open dialer with number
-              const channel = MethodChannel('com.example.hello_world/phone');
-              channel.invokeMethod('callNumber', {'number': '+1234567890'});
-            },
-            child: const Text('Call +1234567890'),
-          ),
+          child: Text('Calling...'),
         ),
       ),
     );
